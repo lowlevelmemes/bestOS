10dbe5cde55c1661ed69768138462df612ff7d38
mintsuki <mintsuki@protonmail.com>
2021-09-07 12:54:58 +0200

    Fix toolchain script

--- toolchain/make_toolchain.sh
+++ toolchain/make_toolchain.sh
@@ -35,8 +35,8 @@ rm -rf build-gcc build-binutils
 mkdir build-binutils
 cd build-binutils
 ../binutils-ia16/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
-make
-make install
+make all-binutils all-ld all-gas
+make install-binutils install-ld install-gas
 cd ..
 
 cd gcc-ia16
@@ -44,9 +44,8 @@ contrib/download_prerequisites
 cd ..
 mkdir build-gcc
 cd build-gcc
-../gcc-ia16/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c --without-headers
+../gcc-ia16/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c --without-headers --disable-multilib
 make all-gcc
-make all-target-libgcc
 make install-gcc
+make all-target-libgcc
 make install-target-libgcc
-cd ..
